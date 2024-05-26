df0e131cfaaa309dc79686207205945e27e58c18
thgdsg <thisangon03@gmail.com>
2024-05-26 16:07:44 -0300

    função "propaganda" adicionada ao bot

--- bot.py
+++ bot.py
@@ -14,7 +14,9 @@ load_dotenv()
 alfabeto = list(string.ascii_lowercase)
 palavraMute = None
 contador = 0
+propaganda = 0
 palavrasMax = 100
+mensagem_block = False
 trocaPalavra = True
 # Trocar caso necessário
 TOKEN = os.getenv('DISCORD_TOKEN') # token do bot
@@ -50,7 +52,7 @@ async def getNewWord():
     palavraMute = str.lower(newWord[indexAleatorio].text)
     print(f"Palavra foi trocada para: {palavraMute}")
 
-@tree.command(name = "novapalavra", description="Busca uma nova palavra aleatória no dicionário")
+@tree.command(name = "novapalavra", description="[ADM] Busca uma nova palavra aleatória no dicionário")
 async def novapalavra(interaction: discord.Interaction):
     if interaction.user.guild_permissions.moderate_members and interaction.guild_id == int(MENES_SUECOS):
         global contador
@@ -61,7 +63,7 @@ async def novapalavra(interaction: discord.Interaction):
     else:
         await interaction.response.send_message("Você não tem permissões suficientes", ephemeral=True)
 
-@tree.command(name = "redefinepalavra", description="Coloca a palavra atual como NULL, nenhuma palavra dará timeout")
+@tree.command(name = "redefinepalavra", description="[ADM] Coloca a palavra atual como NULL, nenhuma palavra dará timeout")
 async def redefinepalavra(interaction: discord.Interaction):
     if interaction.user.guild_permissions.moderate_members and interaction.guild_id == int(MENES_SUECOS):
         global palavraMute
@@ -71,7 +73,7 @@ async def redefinepalavra(interaction: discord.Interaction):
     else:
         await interaction.response.send_message("Você não tem permissões suficientes", ephemeral=True)
 
-@tree.command(name = "mostrapalavra", description="Mostra a palavra atual que dá timeout")
+@tree.command(name = "mostrapalavra", description="[ADM] Mostra a palavra atual que dá timeout")
 async def mostrapalavra(interaction: discord.Interaction):
     if interaction.user.guild_permissions.moderate_members and interaction.guild_id == int(MENES_SUECOS):
         global palavraMute
@@ -83,7 +85,7 @@ async def mostrapalavra(interaction: discord.Interaction):
     else:
         await interaction.response.send_message("Você não tem permissões suficientes", ephemeral=True)
 
-@tree.command(name = "escolhepalavra", description="Define manualmente uma palavra para dar timeout")
+@tree.command(name = "escolhepalavra", description="[ADM] Define manualmente a palavra que causa timeout")
 async def escolhepalavra(interaction: discord.Interaction, novapalavra: str):
     if interaction.user.guild_permissions.moderate_members and interaction.guild_id == int(MENES_SUECOS):
         global palavraMute
@@ -93,17 +95,17 @@ async def escolhepalavra(interaction: discord.Interaction, novapalavra: str):
     else:
         await interaction.response.send_message("Você não tem permissões suficientes", ephemeral=True)
 
-@tree.command(name = "escolhenummensagens", description="Define o número de mensagens lidas para redefinir a palavra que da timeout")
-async def escolhenumpalavras(interaction: discord.Interaction, numeropalavras: int):
+@tree.command(name = "escolhenummensagens", description="[ADM] Define o número de mensagens lidas para redefinir a palavra que da timeout")
+async def escolhenumpalavras(interaction: discord.Interaction, numeromensagens: int):
     if interaction.user.guild_permissions.moderate_members and interaction.guild_id == int(MENES_SUECOS):
         global palavrasMax
-        palavrasMax = numeropalavras
+        palavrasMax = numeromensagens
         await interaction.response.send_message(f"Agora o bot vai trocar de palavra a cada {palavrasMax} mensagens", ephemeral=True)
         print(f"Número de mensagens para trocar a palavra redefinido para {palavrasMax}")
     else:
         await interaction.response.send_message("Você não tem permissões suficientes", ephemeral=True)
 
-@tree.command(name = "mantempalavra", description="Liga/Desliga a função de trocar palavra quando alguém fala")
+@tree.command(name = "mantempalavra", description="[ADM] Liga/Desliga a função de trocar palavra ao ler um número X de mensagens")
 async def mantempalavra(interaction: discord.Interaction):
     if interaction.user.guild_permissions.moderate_members and interaction.guild_id == int(MENES_SUECOS):
         global trocaPalavra
@@ -138,7 +140,7 @@ async def significado(interaction: discord.Interaction, palavra: str):
         await interaction.response.send_message("Server não permitido", ephemeral=True)
 
 # meme command
-@tree.command(name = "mensagemdivina", description="Mensagem dos deuses inspirada no TempleOS")
+@tree.command(name = "mensagemdivina", description="[ADM] Mensagem dos deuses inspirada no TempleOS")
 async def mensagemdivina(interaction: discord.Interaction, numeropalavras: int):
     if interaction.user.guild_permissions.moderate_members and interaction.guild_id == int(MENES_SUECOS):
         fraseAleatoria = ""
@@ -162,7 +164,7 @@ async def mensagemdivina(interaction: discord.Interaction, numeropalavras: int):
 
 @client.event
 async def on_message(message):
-    global palavrasMax
+    global palavrasMax, propaganda, mensagem_block
     if client.user.id != message.author.id:
         if palavraMute != None and palavraMute in str.lower(message.content):
             server = client.get_guild(int(MENES_SUECOS))
@@ -173,6 +175,9 @@ async def on_message(message):
             if member.guild_permissions.moderate_members:
                 print(f"User {message.author.name} com permissão de ADM falou a palavra proibida")
                 await message.channel.send(f"Sem graça, o ADM falou a palavra proibida...")
+                if trocaPalavra == True:
+                    await getNewWord()
+                    print(f"Motivo: Falaram a palavra proibida")
             else:
                 print(f"User {message.author.name} foi mutado")
                 await member.timeout(duration, reason="Falou a palavra proibida do dia")
@@ -182,7 +187,7 @@ async def on_message(message):
                 if trocaPalavra == True:
                     await message.channel.send(f"Parabéns! Você falou a palavra proibida do dia! A palavra é: {palavraMute}\nSeu prêmio é {duration} de Timeout!\nA palavra foi redefinida")
                     await getNewWord()
-                    print(f"Motivo: Falaram a palavra")
+                    print(f"Motivo: Falaram a palavra proibida")
                 else:
                     await message.channel.send(f"Parabéns! Você falou a palavra proibida do dia! A palavra é: {palavraMute}\nSeu prêmio é {duration} de Timeout!")
         #elif TOJAO in message.content:
@@ -195,10 +200,47 @@ async def on_message(message):
                 #contador = 0
         else:
             contador += 1
+            propaganda += 1
             if contador >= palavrasMax and trocaPalavra == True:
                 await getNewWord()
                 print(f"Motivo: atingiu {palavrasMax} mensagens sem a palavra")
                 contador = 0
+            if propaganda >= 25:
+                opcoes_propaganda = {
+                    "# Entre no melhor servidor de todos! \n<https://discord.gg/gou>" : "images/gou.jpg",
+                    "# Não perca! \nPromoções todo dia na <https://amazon.com.br>" : "images/amazon.jpg",
+                    "# Nova season de Fortnite em breve! \nBaixe grátis em <https://fortnite.com>" : "images/fortnite.jpg",
+                    "# Siga a página nas redes sociais! \n<https://youtube.com/@MENESSUECOSS>\n<https://web.facebook.com/MenesSuecos>\n<https://www.instagram.com/mene.sueco>" : "images/menes_suecos.png",
+                    "# Quer aprender a programar? \nAcesse <https://www.codecademy.com> e comece agora!" : "images/codecademy.png",
+                    "# Ouçam o novo álbum da Taylor Swift, a rainha do pop! \nhttps://open.spotify.com/album/5H7ixXZfsNMGbIE5OBSpcb?si=AcDe8Oy7QSSNSVN2U170UA" : "images/taylor_swift.jpg",
+                    "# Hora de acordar quarentena! \n<@&1194720159416467527> <@&1194723205022232637>" : "images/acorda.png",
+                    "# Quer aprender a desenhar? \nAcesse <https://www.skillshare.com> e comece agora!" : "images/skillshare.jpg",
+                    "# Crie uma conta na melhor rede social! \n<https://tiktok.com>" : "images/tiktok.png"
+                }
+                propaganda = 0
+                random_message, random_file = random.choice(list(opcoes_propaganda.items()))
+                print(f"Propaganda enviada, bloqueando chat")
+                sent_message = await message.channel.send(f"{random_message}", file=discord.File(random_file))
+                mensagem_block = sent_message
+                await sent_message.add_reaction("✅")  # Add a "✅" reaction to the message
+                await message.channel.set_permissions(message.guild.default_role, send_messages=False)  # Remove everyone's permissions to send messages in the channel
+
+
+@client.event
+async def on_reaction_add(reaction, user):
+    global mensagem_block
+    react_message = reaction.message
+    if react_message == mensagem_block:
+        mensagem_block = await react_message.channel.fetch_message(react_message.id)
+        for reaction in mensagem_block.reactions:
+            if reaction.count > 4:
+                print(f'Reaction: {reaction.emoji} | Count: {reaction.count} | Deletando mensagem de propaganda e liberando chat')
+                await mensagem_block.delete()
+                await mensagem_block.channel.set_permissions(mensagem_block.guild.default_role, send_messages=True) 
+                mensagem_block = False
+                break
+    else:
+        pass
 
 client.run(TOKEN)
 
